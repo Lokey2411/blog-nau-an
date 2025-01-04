6fff1080fb6199b8e99652820c5fc39a8cfeb2ab
garan1610 <134696618+garan1610@users.noreply.github.com>
2025-01-04 18:07:30 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
 	return (
 		<div className=" border-t  ">
 			<div className="flex justify-between w-1/2 m-auto mt-6">
-				<p className="text-gray-500 pl-8">2023</p>
+				<p className="text-gray-500 pl-8">2024</p>
 				<div className="w-1/5">
 					<ContactInfo />
 				</div>
