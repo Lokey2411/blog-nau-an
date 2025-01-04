49bdad1164d7bbc448f14573eb09d63df0164eda
garan1610 <134696618+garan1610@users.noreply.github.com>
2025-01-04 17:38:05 +0700

    Update index.ts

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,21 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyCoHY2klxucWPaFMmXLOsB_2IK_KTfZGaM",
-	authDomain: "blog-nau-an.firebaseapp.com",
-	projectId: "blog-nau-an",
-	storageBucket: "blog-nau-an.appspot.com",
-	messagingSenderId: "539416428616",
-	appId: "1:539416428616:web:b20dbff8dd86014f42d87d",
-	measurementId: "G-RDK6T9M1BF",
+  apiKey: "AIzaSyByuVprkemrWqBkDQCZvwIf6lYetwzrzc0",
+  authDomain: "foodblog-b6edb.firebaseapp.com",
+  projectId: "foodblog-b6edb",
+  storageBucket: "foodblog-b6edb.firebasestorage.app",
+  messagingSenderId: "917020041122",
+  appId: "1:917020041122:web:21c2cd636b32b893651cfb",
+  measurementId: "G-NV49MQFMSY"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const firestore = getFirestore(app);
+const analytics = getAnalytics(app);
