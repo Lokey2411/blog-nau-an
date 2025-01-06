8f92ddab8169800c488bbf71304d90c57b8b0ad9
garan1610 <134696618+garan1610@users.noreply.github.com>
2025-01-06 23:17:14 +0700

    Update Introduction.tsx

--- src/screens/Introduction.tsx
+++ src/screens/Introduction.tsx
@@ -20,7 +20,7 @@ export default function Introduction() {
 					<UnderlineText>
 						<p className="text-2xl">Giới thiệu</p>
 					</UnderlineText>
-					<p>Tôi là Nguyễn Vũ Nhật Minh, một học sinh cấp 3 tại trường THPT Chuyên Ngoại Ngữ, Hà Nội. Tôi là một người có đam mê sâu sắc về nấu ăn và ẩm thực. Cái tên "Garan Food Blog" chỉ đơn giản là tên con chó nhà tôi (Gà rán) chứ không phải blog chỉ chia sẻ cách nấu gà rán đâu nhá!</p>
+					<p>Tôi là Nguyễn Vũ Nhật Minh, một người Hà Nội. Tôi là một người có đam mê sâu sắc về nấu ăn và ẩm thực. Cái tên "Garan Food Blog" chỉ đơn giản là tên con chó nhà tôi (Gà rán) chứ không phải blog chỉ chia sẻ cách nấu gà rán đâu nhá!</p>
 					<p>Blog cá nhân này được tôi tạo ra với mục đích để chia sẻ các công thức nấu ăn ngon và đơn giản. Các món ăn vô cùng đặc sắc từ các món nước ngoài đến các món Việt Nam truyền thống. Qua đây, tôi hi vọng truyền cho mọi người cảm hứng để vào nhà bếp và trải nghiệm văn hóa của các quốc gia hoặc nếm lại mùi vị đặc trưng của quê hương.</p>
 				</div>
 			</div>
